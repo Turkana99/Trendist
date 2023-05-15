25b6f3bd046ab859ab9264e3bbdffac5d4342d40
Turkana Mammadova <mmmmadova.turkana@gmail.com>
2023-05-15 08:50:47 +0400

    New app Trendist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { FagComponent } from './fag/fag.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'article-details', component: ArticleDetailsComponent },
+  { path: 'fag', component: FagComponent },
+  { path: '', redirectTo:'/home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { FagComponent } from './fag/fag.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ArticlesComponent,
+    ArticleDetailsComponent,
+    FagComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
